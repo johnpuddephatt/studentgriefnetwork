f63f04365ee7d7738743e0fc54d399b329af3ad9
John Puddephatt <john@jdp.org.uk>
2021-10-27 19:03:18 +0100

    footer z index not needed

--- resources/views/partials/home/newsletter.blade.php
+++ resources/views/partials/home/newsletter.blade.php
@@ -9,10 +9,23 @@
       </h2>
       <p class="mb-8 text-xl text-blue font-kayaksans">Sign up to read our latest blog posts and find out about our
         events</p>
-      <form class="flex">
+      <form class="flex" method="POST" action="https://studentgriefnetwork.us5.list-manage.com/subscribe/post">
+        <input type="hidden" name="u" value="47c1a5b8d56cbb03f04b48aad">
+        <input type="hidden" name="id" value="e30db2587d">
+        <div class="absolute left-[9999px]" aria-label="Please leave the following three fields empty">
+          <label for="b_name">Name: </label>
+          <input type="text" name="b_name" tabindex="-1" value="" placeholder="Freddie" id="b_name">
+
+          <label for="b_email">Email: </label>
+          <input type="email" name="b_email" tabindex="-1" value="" placeholder="youremail@gmail.com" id="b_email">
+
+          <label for="b_comment">Comment: </label>
+          <textarea name="b_comment" tabindex="-1" placeholder="Please comment" id="b_comment"></textarea>
+        </div>
         <input
           class="block p-3 bg-transparent border-t-0 border-b border-l-0 border-r-0 w-96 border-blue placeholder-blue-light"
-          type="text" name="email" placeholder="Enter your email address...">
+          type="text" name="MERGE0" placeholder="Enter your email address...">
+        <input type="hidden" name="mc_signupsource" value="homepage">
         <input
           class="block px-8 py-3 subpixel-antialiased font-semibold lowercase bg-transparent border border-blue text-blue"
           type="submit" />
